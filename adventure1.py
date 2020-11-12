8fb3b6d0a2a7606cab963acdb17ec84423bae9ff
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-11-12 08:30:09 +0000

    Added text adventure start

---
+++ adventure1.py
@@ -0,0 +1,20 @@
+command = ""
+
+while (command != "exit"):
+    print("What now?")
+    command = input()
+
+    if (command == "north"):
+        print("You move north...")
+    elif (command == "south"):
+        print("You move south...")
+    elif (command == "east"):
+        print("You move east...")
+    elif (command == "west"):
+        print("You move west...")
+    elif (command == "exit"):
+        pass
+    else:
+        print("I don't understand " + command)
+
+print("Goodbye!")
